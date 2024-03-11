1b882f9e17a15d3cee4abd65e8d22de07b21424b
BALAND Jérome <baland.jerome@gmail.com>
2024-03-11 20:32:02 +0100

    JS : classe input suivant select

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+let list;
 
+function displayList(id) {
+  const ul = document.getElementById(id);
+  ul.style.display = "block";
+  ul.innerHTML += `
+    <li>${input.value}</li>
+    `;
+}
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (input.value.length == 0) {
+    alert("Aucun produit écrit");
+  } else if (select.value === "rayon") {
+    alert("Rayon non selectionné");
+  } else {
+    noList.style.display = "none";
+    displayList(select.value);
+    input.value = "";
+    select.value = "rayon";
+  }
+});
